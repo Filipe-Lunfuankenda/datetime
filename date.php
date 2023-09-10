676dd4fdde93bd62ae446b8585d4e72c8ed9f3a3
Filipe Lunfuankenda <135343917+Filipe-Lunfuankenda@users.noreply.github.com>
2023-09-10 19:46:21 +0100

    Add files via upload

---
+++ date.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Data e Hora Atual em PHP</title>
+    <style>
+        body {
+            background-image: linear-gradient(to bottom, orange, red);
+            text-align: center;
+            height: 100vh;
+            font-family: Arial, Helvetica, sans-serif;
+            color: white;
+        }
+
+        p {
+            font-size: 5em;
+            font-family: ds-digital, Arial, Helvetica, sans-serif;
+        }
+    </style>
+</head>
+
+<body>
+    <h1>Script to get the Current Date and Time</h1>
+    <?php
+    date_default_timezone_set("Europe/Lisbon"); //You can configure the timezone whatever you want
+    $dataHoraAtual = date("d/m/Y H:i:s");
+    echo "<p>$dataHoraAtual</p>";
+
+    ?>
+</body>
+
+</html>
