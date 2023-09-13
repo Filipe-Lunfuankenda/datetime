0d6cbfe68a462b727046981fcfca83a16c985136
Filipe Lunfuankenda <135343917+Filipe-Lunfuankenda@users.noreply.github.com>
2023-09-13 11:09:30 +0100

    Update date.php

--- date.php
+++ date.php
@@ -1,6 +1,5 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,14 +19,12 @@
         }
     </style>
 </head>
-
 <body>
     <h1>Script to get the Current Date and Time</h1>
     <?php
-    date_default_timezone_set("Europe/Lisbon"); //You can configure the timezone whatever you want
-    $dataHoraAtual = date("d/m/Y H:i:s");
-    echo "<p>$dataHoraAtual</p>";
-
+        date_default_timezone_set("Europe/Lisbon"); //You can configure the timezone whatever you want
+        $dataHoraAtual = date("d/m/Y H:i:s");
+        echo "<p>$dataHoraAtual</p>";
     ?>
 </body>
 
